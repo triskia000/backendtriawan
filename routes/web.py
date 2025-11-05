f807f4d2a8ca2982c248d0f7e58f860baba6f43e
triskia000 <amandatriskia29@gmail.com>
2025-11-05 13:15:29 +0700

    push database

--- routes/web.py
+++ routes/web.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from controllers.menu_controllers import get_all_menu, add_menu, update_menu, delete_menu
 
 web = Blueprint('web', __name__)
-web.route('/menu', methods=['GET'])(get_all_menu)
+web.route("/", methods=['GET'])(get_all_menu)
 web.route("/menu", methods=['POST'])(add_menu)
 web.route("/menu/<int:id_menu>", methods=['PUT'])(update_menu)
 web.route("/menu/<int:id_menu>", methods=['DELETE'])(delete_menu)
